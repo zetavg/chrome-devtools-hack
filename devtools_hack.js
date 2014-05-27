1f0ab505d5b2d803c623f4c9595270cf20bc0b8f
Neson <neson@dex.tw>
2014-05-28 05:01:39 +0800

    check if elements exist before running the script

--- devtools_hack.js
+++ devtools_hack.js
@@ -1,106 +1,124 @@
 if (location.protocol === 'chrome-devtools:') {
 
-	setTimeout(function () {
+  __hack_init = false;
 
-    console.log('hello?');
+  function __hack() {
 
-		var __hack_createHTML = function (htmlStr) {
-			var frag = document.createDocumentFragment(),
-				temp = document.createElement('div');
-			temp.innerHTML = htmlStr;
-			while (temp.firstChild) {
-				frag.appendChild(temp.firstChild);
-			}
-			return frag;
-		};
+    // Check if devtools is fully initialized
+    if (document.getElementsByClassName('split-view-contents')[0] && document.getElementsByClassName('sidebar-pane-toolbar')[0] && document.getElementsByClassName('matched-styles')[0] && !__hack_init) {
 
-		var __hack_fuzzyMatch = function (str, pattern) {
-			pattern = pattern.split("").reduce(function (a,b) { return a+".*"+b; });
-			return (new RegExp(pattern)).test(str);
-		};
+      console.log('hello?');
 
+      var __hack_createHTML = function (htmlStr) {
+        var frag = document.createDocumentFragment(),
+          temp = document.createElement('div');
+        temp.innerHTML = htmlStr;
+        while (temp.firstChild) {
+          frag.appendChild(temp.firstChild);
+        }
+        return frag;
+      };
 
-		/* Custom Shortcuts */
-		document.body.onkeydown = function(e) {
-			switch (e.keyCode) {
-				case 76:
-					if (e.altKey && (e.metaKey || e.ctrlKey)) {
-						if (document.getElementsByClassName('toggled-left')[0])
-							document.getElementsByClassName('toggled-left')[0].click();
-						else if (document.getElementsByClassName('toggled-right')[0])
-							document.getElementsByClassName('toggled-right')[0].click();
-						else if (document.getElementsByClassName('toggled-bottom')[0])
-							document.getElementsByClassName('toggled-bottom')[0].click();
-						else if (document.getElementsByClassName('toggled-bottom')[0])
-							document.getElementsByClassName('toggled-bottom')[0].click();
-						else if (document.getElementsByClassName('toggled-undocked')[0])
-							document.getElementsByClassName('toggled-undocked')[0].click();
-					}
-					break;
-
-        case 67:
-          document.getElementsByClassName('node-search-status-bar-item')[0].click();
-          break;
-			}
-		};
+      var __hack_fuzzyMatch = function (str, pattern) {
+        pattern = pattern.split("").reduce(function (a,b) { return a+".*"+b; });
+        return (new RegExp(pattern)).test(str);
+      };
 
 
-		/* CSS Quick-searching */
-		var searchBox = __hack_createHTML('<style> input::-webkit-input-placeholder, textarea::-webkit-input-placeholder { color: rgba(128,128,128, .5); } .css-search-box:focus { border: 1px solid rgba(128,128,128, .5); } .styles-element-state-pane { padding-top: 20px !important; height: 56px !important; }</style><input class="css-search-box monospace" type="text" placeholder="Find" style="float: right; background-color: transparent; outline: none !important; border: 1px solid rgba(128,128,128, .32); background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAGFBMVEUAAACRlZyRlZySlZuRlZuRlJyQlZ2SlZwapm9KAAAAB3RSTlMAjd6rQGAZKun8xQAAAKdJREFUeNqdk0EOxDAIAwED+f+P9xBaBEGKtr4RD2CJlkJuEFkCVhrFsh6JnbaGHcLhryapvqczEzGf1ckNUVCKd49HaZvwBPpMbUEtG8rLWyJW9lDaq56KC+AFsAJkxBoT1wm3DFYqFGCVHo0yhRZLGmF5jHzInc7nOREnZlPbdvK55BBTyq8E4UroiwgrJoLcGIjfYiamlfhI/LeFLsQEjB/CTIiAfogFCkGFlhykAAAAAElFTkSuQmCC); background-repeat: no-repeat; background-size: auto 80%; background-position: 3px 2px; padding-left: 18px !important; width: 40%;">');
-		document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].insertBefore(searchBox, document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].childNodes[99]);
+      /* Custom Shortcuts */
+      document.body.onkeydown = function(e) {
+        switch (e.keyCode) {
+          case 76:
+            if (e.altKey && (e.metaKey || e.ctrlKey)) {
+              if (document.getElementsByClassName('toggled-left')[0])
+                document.getElementsByClassName('toggled-left')[0].click();
+              else if (document.getElementsByClassName('toggled-right')[0])
+                document.getElementsByClassName('toggled-right')[0].click();
+              else if (document.getElementsByClassName('toggled-bottom')[0])
+                document.getElementsByClassName('toggled-bottom')[0].click();
+              else if (document.getElementsByClassName('toggled-bottom')[0])
+                document.getElementsByClassName('toggled-bottom')[0].click();
+              else if (document.getElementsByClassName('toggled-undocked')[0])
+                document.getElementsByClassName('toggled-undocked')[0].click();
+            }
+            break;
 
-		var __hack_cssSearch = function () {
-			var s = document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].value;
-			var styleType = ['navigable', 'read-only'];
-			for (t=0; t<styleType.length; t++) {
-				var styles = document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('styles-pane')[0].getElementsByClassName(styleType[t]);
-
-				if (s !== '') {
-					for (i=0; i<styles.length; i++) {
-						properties = styles[i].getElementsByClassName('webkit-css-property');
-						k = properties.length;
-						for (j=0; j<properties.length; j++) {
-							if (__hack_fuzzyMatch(properties[j].innerHTML, s) === false) {
-								properties[j].parentNode.style.display = "none";
-								k--;
-							} else {
-								properties[j].parentNode.style.display = "list-item";
-							}
-						}
-						if (k <= 0) {
-							styles[i].style.display = "none";
-						}
-					}
-
-					for (i=0; i<properties.length; i++) {
-						if (__hack_fuzzyMatch(properties[i].innerHTML, s) === false) {
-							properties[i].parentNode.style.display = "none";
-						} else {
-							properties[i].parentNode.style.display = "list-item";
-						}
-					}
-
-				} else {
-					for (i=0; i<styles.length; i++) {
-						styles[i].style.display = "block";
-						properties = styles[i].getElementsByClassName('webkit-css-property');
-						for (j=0; j<properties.length; j++) {
-							properties[j].parentNode.style.display = "list-item";
-						}
-					}
-				}
-			}
-		};
-
-		document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('change', __hack_cssSearch, false);
-		document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('keyup', __hack_cssSearch, false);
-		document.getElementsByClassName('split-view-contents')[0].getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('paste', __hack_cssSearch, false);
+          case 67:
+            document.getElementsByClassName('node-search-status-bar-item')[0].click();
+            break;
+        }
+      };
 
 
-		/* Toggle the Drawer twice to avoid incorrect element position */
-		document.getElementsByClassName('console-status-bar-item')[0].click();
-		document.getElementsByClassName('console-status-bar-item')[0].click();
+      /* CSS Quick-searching */
+      var searchBox = __hack_createHTML('<style> input::-webkit-input-placeholder, textarea::-webkit-input-placeholder { color: rgba(128,128,128, .5); } .css-search-box:focus { border: 1px solid rgba(128,128,128, .5); } .styles-element-state-pane { padding-top: 20px !important; height: 56px !important; }</style><input class="css-search-box monospace" type="text" placeholder="Find" style="float: right; background-color: transparent; outline: none !important; border: 1px solid rgba(128,128,128, .32); background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAGFBMVEUAAACRlZyRlZySlZuRlZuRlJyQlZ2SlZwapm9KAAAAB3RSTlMAjd6rQGAZKun8xQAAAKdJREFUeNqdk0EOxDAIAwED+f+P9xBaBEGKtr4RD2CJlkJuEFkCVhrFsh6JnbaGHcLhryapvqczEzGf1ckNUVCKd49HaZvwBPpMbUEtG8rLWyJW9lDaq56KC+AFsAJkxBoT1wm3DFYqFGCVHo0yhRZLGmF5jHzInc7nOREnZlPbdvK55BBTyq8E4UroiwgrJoLcGIjfYiamlfhI/LeFLsQEjB/CTIiAfogFCkGFlhykAAAAAElFTkSuQmCC); background-repeat: no-repeat; background-size: auto 80%; background-position: 3px 2px; padding-left: 18px !important; width: 40%;">');
 
-    console.log('hello.');
-	}, 2400);
+
+      document.getElementsByClassName('sidebar-pane-toolbar')[0].insertBefore(searchBox, document.getElementsByClassName('sidebar-pane-toolbar')[0].childNodes[99]);
+
+
+      var __hack_cssSearch = function () {
+        var s = document.getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].value;
+        var styleType = ['navigable', 'read-only'];
+        for (t=0; t<styleType.length; t++) {
+          var styles = document.getElementsByClassName('styles-pane')[0].getElementsByClassName(styleType[t]);
+
+          if (s !== '') {
+            for (i=0; i<styles.length; i++) {
+              properties = styles[i].getElementsByClassName('webkit-css-property');
+              k = properties.length;
+              for (j=0; j<properties.length; j++) {
+                if (__hack_fuzzyMatch(properties[j].innerHTML, s) === false) {
+                  properties[j].parentNode.style.display = "none";
+                  k--;
+                } else {
+                  properties[j].parentNode.style.display = "list-item";
+                }
+              }
+              if (k <= 0) {
+                styles[i].style.display = "none";
+              }
+            }
+
+            for (i=0; i<properties.length; i++) {
+              if (__hack_fuzzyMatch(properties[i].innerHTML, s) === false) {
+                properties[i].parentNode.style.display = "none";
+              } else {
+                properties[i].parentNode.style.display = "list-item";
+              }
+            }
+
+          } else {
+            for (i=0; i<styles.length; i++) {
+              styles[i].style.display = "block";
+              properties = styles[i].getElementsByClassName('webkit-css-property');
+              for (j=0; j<properties.length; j++) {
+                properties[j].parentNode.style.display = "list-item";
+              }
+            }
+          }
+        }
+      };
+
+      document.getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('change', __hack_cssSearch, false);
+      document.getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('keyup', __hack_cssSearch, false);
+      document.getElementsByClassName('sidebar-pane-toolbar')[0].getElementsByClassName('css-search-box')[0].addEventListener('paste', __hack_cssSearch, false);
+
+
+      /* Toggle the Drawer twice to avoid incorrect element position */
+      document.getElementsByClassName('console-status-bar-item')[0].click();
+      document.getElementsByClassName('console-status-bar-item')[0].click();
+
+      __hack_init = true
+      console.log('hello.');
+    } else {
+      console.log('resting.')
+      setTimeout(__hack, 1200);
+    }
+  }
+
+  setTimeout(__hack, 2400);
+  setTimeout(__hack, 3600);
+  setTimeout(__hack, 5200);
+  setTimeout(__hack, 9900);
 }
